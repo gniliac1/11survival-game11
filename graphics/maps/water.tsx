9b13ff8af4ffde786d8cbc1626bd6917a320c8cd
Cailing Fu <cailingfu@hotmail.de>
2018-12-22 23:22:57 +0100

    .tmx map

---
+++ graphics/maps/water.tsx
@@ -0,0 +1,124 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="water" tilewidth="32" tileheight="32" tilecount="18" columns="3">
+ <image source="../tiles/water.png" width="96" height="192"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="2" x="0.875" y="20.5">
+    <polygon points="0,0 1.875,-2.5 8.875,-3 8,-4.5 4,-8.375 6.625,-11.625 10,-13.25 20.25,-12.5 26.375,-10.75 28.75,-6.75 28.375,-2.75 25.375,1.75 23,3.875 20,4 16.625,7.25 12.75,8.375 5.5,7.625 2.5,5.125 0.25,1.625"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index">
+   <object id="1" x="18.875" y="31.875">
+    <polygon points="0,0 2.75,-7.375 8.375,-10.25 13.25,-14.125 13.125,-31.875 -18.75,-31.75 -18.875,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index">
+   <object id="1" x="0.25" y="15.875">
+    <polygon points="0,0 4.25,0.75 8.375,8 12.125,9.125 14.875,12 14.875,15.875 31.75,16 31.75,-15.875 -0.25,-15.875"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index">
+   <object id="1" x="7.5" y="15.125">
+    <polygon points="0,0 -0.25,-4.25 3.625,-7.25 9.625,-7 14.5,-6.5 17.625,-3.375 17.625,1.875 16.875,6.875 11.125,11.875 6.25,13.375 0.875,11.625 -1.625,8.5 -1.125,3"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index">
+   <object id="1" x="18.875" y="0.25">
+    <polygon points="0,0 1.875,6.25 0.875,10.875 7.75,13 13.125,13 13.25,31.625 -18.875,31.625 -18.875,-0.25"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="12.125">
+    <polygon points="0,0 5.5,2.25 7.5,1.375 9.875,-2.625 12.375,-5.375 12.375,-12.125 32.125,-12.125 32.125,19.875 0,19.875"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index">
+   <object id="1" x="12" y="31.75">
+    <polygon points="0,0 4.875,-4.375 8.5,-8.875 11.875,-14.25 18.125,-18.75 19.875,-18.875 20,0.125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index">
+   <object id="1" x="0.125" y="12.125">
+    <polygon points="0,0 11.25,1.875 18.875,-1.125 31.625,-0.875 31.875,20.125 -0.125,19.75"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="11.375">
+    <polygon points="0,0 7.625,3.875 14.5,12.875 19.125,19.125 19.75,20.625 0.125,20.5"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index">
+   <object id="1" x="12.5" y="0.125">
+    <polygon points="0,0 1.375,7.375 3.25,14.75 4.625,20.875 2.375,27 0,31.75 19.5,31.875 19.5,-0.125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0.125">
+    <polygon points="0,0 -0.125,31.875 31.875,31.875 31.875,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <objectgroup draworder="index">
+   <object id="1" x="18.5" y="0">
+    <polygon points="0,0 -2,5.375 -3,8.625 -2,13.25 1.125,18 4.625,22.625 3.25,26.75 1,31.875 -18.5,32 -18.5,0.125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup draworder="index">
+   <object id="1" x="12.375" y="0.125">
+    <polygon points="0,0 3.75,3.875 12.625,14.25 14.125,15.75 17.75,18.25 19.625,18.25 19.75,-0.125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.125" y="18.75">
+    <polygon points="0,0 4.875,-2.5 10.375,-0.75 16.25,-0.875 20.875,-3.75 24,-3.25 28.875,-0.625 32.125,-0.5 32.125,-18.625 0.25,-18.875"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <objectgroup draworder="index">
+   <object id="1" x="0.125" y="17.625">
+    <polygon points="0,0 8.25,-0.5 15,-6.125 15,-11.875 17.875,-15.25 19.75,-17.75 -0.125,-17.75"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup draworder="index">
+   <object id="1" x="-0.125" y="0" width="32.125" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <objectgroup draworder="index">
+   <object id="4" x="0.125" y="0" width="31.875" height="32.125"/>
+  </objectgroup>
+ </tile>
+</tileset>
