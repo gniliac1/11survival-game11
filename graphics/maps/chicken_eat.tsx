46578fbd827b0c50ba79d4a141f0c2b9074b305b
Cailing Fu <cailingfu@hotmail.de>
2019-08-31 19:34:31 +0200

    Sprites *bisschen* aufgeräumt

---
+++ graphics/maps/chicken_eat.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="chicken_eat" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="../sprites/animals/chicken_eat.png" width="128" height="128"/>
+</tileset>
---
+++ graphics/maps/cow_eat.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="cow_eat" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="../sprites/animals/cow_eat.png" width="512" height="512"/>
+</tileset>
