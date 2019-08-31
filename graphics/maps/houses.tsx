6825dbe9a4dd30f9b2dce972edd79df01bfd0f39
Cailing Fu <cailingfu@hotmail.de>
2019-08-31 14:52:43 +0200

    haus Tests

---
+++ graphics/maps/houses.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="houses" tilewidth="32" tileheight="32" tilecount="1536" columns="32">
+ <image source="../tiles/houses/houses.png" width="1024" height="1536"/>
+</tileset>
