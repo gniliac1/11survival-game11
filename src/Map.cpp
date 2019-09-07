80611f622af87cf487ea541fef04a375e2c12952
Daniel Wolff <daniel.wolff@rwth-aachen.de>
2019-09-07 23:20:36 +0200

    Added first draft for the map class

---
+++ src/Map.cpp
@@ -0,0 +1,66 @@
+#include "pch.h"
+#include "Map.h"
+
+
+Map::Map(const std::string& mapPath, const sf::FloatRect& windowRect)
+	: _map(std::make_unique<tmx::MapLoader>(mapPath)),
+	 _windowRect(windowRect)
+{
+}
+
+Map::~Map()
+{
+}
+
+bool Map::checkCollision(const sf::FloatRect& playerRect)
+{
+	// query the quad tree with the bounding box of the player
+	_mapObjectsOnScreen = _map->queryQuadTree(playerRect);
+
+	// check whether the player rect is inside the window rect
+	if (playerRect.left < _windowRect.left || playerRect.left + playerRect.width > _windowRect.left + _windowRect.width ||
+		playerRect.top < _windowRect.top || playerRect.top + playerRect.height > _windowRect.top + _windowRect.height)
+	{
+		return true;
+	}
+
+	// iterate over all objects in the map
+	for (const tmx::MapObject* object : _mapObjectsOnScreen)
+	{
+		// check whether the collision boxes intersect
+		if (object->getAABB().intersects(playerRect))
+		{
+			// collision detected
+			return true;
+		}
+	}
+}
+
+const std::vector<tmx::MapLayer>& Map::getLayers() const
+{
+	return _map->getLayers();
+}
+
+bool Map::load(const std::string& mapName)
+{
+	// load the map
+	if (_map->load(mapName))
+	{
+		// update the quad tree with the current window size
+		_map->updateQuadTree(_windowRect);
+
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void Map::setWindowRect(const sf::FloatRect & windowRect)
+{
+	_windowRect = windowRect;
+
+	// update the quad tree with the new window size
+	_map->updateQuadTree(_windowRect);
+}
---
+++ src/Map.h
@@ -0,0 +1,41 @@
+#pragma once
+
+// sfml tmxloader
+#include<tmx/MapLoader.hpp>
+
+class Map
+{
+public:
+
+	// Construct a map by the path to the map files and the size of the window
+	Map(const std::string& mapPath, const sf::FloatRect& windowRect);
+
+	// Empty standard destructor
+	~Map();
+
+	// Check for collisions of the player and map objects
+	bool checkCollision(const sf::FloatRect& playerRect);
+
+	// Returns vector containing each layer of the map
+	const std::vector<tmx::MapLayer>& getLayers() const;
+
+	// Loads a tmx map from file
+	bool load(const std::string& mapName);
+
+private:
+
+	// Map loader for loading maps in tmx format
+	std::unique_ptr<tmx::MapLoader> _map;
+
+	// Vector containing all objects in the part of he map which is shown on screen
+	std::vector<tmx::MapObject*> _mapObjectsOnScreen;
+
+	// Current size of the screen
+	sf::FloatRect _windowRect;
+
+public:
+
+	// Set the rect of the screen 
+	void setWindowRect(const sf::FloatRect& windowRect);
+};
+
